81ad39a068462e269261e4d9cbfb2857f214abfd
Riley Paul <rileypaul96@gmail.com>
2024-02-29 11:02:24 -0800

    clean up app and db

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,7 @@
-import { useMutation, useQuery } from "react-query";
-import { Button } from "./components/ui/button";
-import { deleteCompletedTodos, getTodos } from "./lib/api";
-import { Collections } from "./lib/types";
-import Todo from "./components/todo";
 import Adder from "./components/adder";
-import { queryClient } from "./lib/query";
 import TodoList from "./components/todo-list";
 
-function App() {
-  const todosQuery = useQuery({
-    queryKey: [Collections.Todos],
-    queryFn: getTodos,
-  });
-
-  const deleteCompleteMutation = useMutation({
-    mutationFn: () => deleteCompletedTodos(),
-    onSuccess: () => {
-      queryClient.invalidateQueries([Collections.Todos]);
-    },
-  });
-
+function App(): ReturnType<React.FC> {
   return (
     <main className="h-screen w-full flex items-center justify-center overflow-hidden">
       <div className="min-h-[40rem] max-w-[25rem] max-h-screen w-full p-4 flex flex-col gap-4">
